b23691ad2213644b8ed0de87a9a6e50bfc0e7528
JakobMenBedre <jakobrasm@viken.no>
2023-11-03 10:42:29 +0100

    Add project files.

---
+++ app 5/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace app_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.tid = new System.Windows.Forms.Label();
+            this.starttrykk = new System.Windows.Forms.Button();
+            this.sekunder = new System.Windows.Forms.Label();
+            this.trykk = new System.Windows.Forms.Label();
+            this.clicks = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Image = global::app_5.Properties.Resources.stop_talking_shit;
+            this.start.Location = new System.Drawing.Point(-109, -99);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(743, 551);
+            this.start.TabIndex = 0;
+            this.start.Text = "trykk for start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // tid
+            // 
+            this.tid.AutoSize = true;
+            this.tid.Location = new System.Drawing.Point(692, 46);
+            this.tid.Name = "tid";
+            this.tid.Size = new System.Drawing.Size(19, 13);
+            this.tid.TabIndex = 1;
+            this.tid.Text = "10";
+            // 
+            // starttrykk
+            // 
+            this.starttrykk.Location = new System.Drawing.Point(661, 146);
+            this.starttrykk.Name = "starttrykk";
+            this.starttrykk.Size = new System.Drawing.Size(111, 66);
+            this.starttrykk.TabIndex = 2;
+            this.starttrykk.Text = "trykk for start";
+            this.starttrykk.UseVisualStyleBackColor = true;
+            this.starttrykk.Click += new System.EventHandler(this.starttrykk_Click);
+            // 
+            // sekunder
+            // 
+            this.sekunder.AutoSize = true;
+            this.sekunder.Location = new System.Drawing.Point(678, 33);
+            this.sekunder.Name = "sekunder";
+            this.sekunder.Size = new System.Drawing.Size(51, 13);
+            this.sekunder.TabIndex = 3;
+            this.sekunder.Text = "sekunder";
+            // 
+            // trykk
+            // 
+            this.trykk.AutoSize = true;
+            this.trykk.Location = new System.Drawing.Point(678, 351);
+            this.trykk.Name = "trykk";
+            this.trykk.Size = new System.Drawing.Size(62, 13);
+            this.trykk.TabIndex = 4;
+            this.trykk.Text = "antall clicks";
+            // 
+            // clicks
+            // 
+            this.clicks.AutoSize = true;
+            this.clicks.Location = new System.Drawing.Point(695, 368);
+            this.clicks.Name = "clicks";
+            this.clicks.Size = new System.Drawing.Size(13, 13);
+            this.clicks.TabIndex = 5;
+            this.clicks.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clicks);
+            this.Controls.Add(this.trykk);
+            this.Controls.Add(this.sekunder);
+            this.Controls.Add(this.starttrykk);
+            this.Controls.Add(this.tid);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label tid;
+        private System.Windows.Forms.Button starttrykk;
+        private System.Windows.Forms.Label sekunder;
+        private System.Windows.Forms.Label trykk;
+        private System.Windows.Forms.Label clicks;
+    }
+}
+
---
+++ app 5/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+
+
+namespace app_5
+{
+    public partial class Form1 : Form
+        
+    {
+        int y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer(); //henter timer sine greier
+        private int x = 10; //x er 10 :)
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (x != 0)
+            {
+                y++;
+                clicks.Text=y.ToString(); //om x ikke er 0 så gjør den y=y+1 og så bytter clicks.Text til den y sin verdi.
+            }
+                
+        }
+
+        private void starttrykk_Click(object sender, EventArgs e)
+        {
+            myTimer.Interval = 1000; //lager en timer med en interval hvert 1000 millisekunder (1 sekund) 
+            myTimer.Tick += UpdateTime; //får UpdateTime til å handle tidstakeren
+            myTimer.Start(); //starter tidstakeren
+        }
+        private void UpdateTime(object sender, EventArgs e)
+        {
+            
+            x--;
+            tid.Text = x.ToString();//kjører x=x-1 (som er 10 i starten og vil da bli mindre og mindre) og deretter setter verdien i tid.Text 
+            if (x == 0)
+            {
+                myTimer.Stop(); //når x enderlig er lik 0 så stopper den tidstakeren
+            }
+        }
+    }
+}
